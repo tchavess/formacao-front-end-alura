5c6b6c46ed4c6f0ed270b9dc98e6253f3caa12ea
Thiago Chaves <thiago.chaves04@gmail.com>
2020-03-27 12:20:32 -0300

    calculo de IMC no front, validacao dos dados reais

--- JavaScript/introducao-javascript/js/principal.js
+++ JavaScript/introducao-javascript/js/principal.js
@@ -1,2 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc  = peso/(altura * altura);
+	tdImc.textContent = imc;
+}else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
+
+
