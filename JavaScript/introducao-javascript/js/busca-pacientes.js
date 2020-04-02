3a989929184bd1ac0e2335967b6b4ede2b583cc8
Thiago Chaves <thiago.chaves04@gmail.com>
2020-04-01 21:48:05 -0300

    Tratamento de status error na adição de pacientes a tabela

--- JavaScript/introducao-javascript/js/busca-pacientes.js
+++ JavaScript/introducao-javascript/js/busca-pacientes.js
@@ -5,15 +5,24 @@ console.log("Buscando pacientes...");
 
 var request =  new XMLHttpRequest();
 
-request.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+request.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
 request.addEventListener("load", function(){
-	var resposta = request.responseText;
-    var pacientes = JSON.parse(resposta);
+	var erroAjax = document.querySelector("#erro-ajax");
 
-    pacientes.forEach(function(paciente) {
-        adicionaPacienteNaTabela(paciente);
-    });
+    if (request.status == 200) {
+	    erroAjax.classList.add("invisivel");
+	    var resposta = request.responseText;
+	    var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
+    } else {
+    	erroAjax.setAttribute("id","mensagens-erro");
+        erroAjax.classList.remove("invisivel");
+
+    }																													        
 });
 
 request.send();
