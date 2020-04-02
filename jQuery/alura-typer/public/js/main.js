6b2b45558ac73486a7fef9a35071003d3f2ea1a8
Thiago Chaves <thiago.chaves04@gmail.com>
2020-04-02 14:16:52 -0300

    feature reinicializa o jogo, refatoramento de codigo, boas praticas de codigo

--- jQuery/alura-typer/public/js/main.js
+++ jQuery/alura-typer/public/js/main.js
@@ -1,17 +1,31 @@
-//acessa o conteudo da classe frase
-var frase = $(".frase").text();
-
-//quebrando a frase por espacos
-var numPalavras = frase.split(" ").length;
-
-//seleciona o span com o tamanho da frase e modifica seu conteudo 
-//para o conteudo de numPalavras
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
 //Selecionamos o campo pelo nome da classe e já associamos o evento input com ele.
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
+
+var tempoInicial = $("#tempo-digitacao").text();
+
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function atualizaTamanhoFrase() {
+   //acessa o conteudo da classe frase
+	var frase = $(".frase").text();
+    
+    //quebrando a frase por espacos
+	var numPalavras = frase.split(" ").length;
+    
+   	//seleciona o span com o tamanho da frase e modifica seu conteudo 
+	//para o conteudo de numPalavras
+	var tamanhoFrase = $("#tamanho-frase");
+	tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    
+    campo.on("input", function() {
 	//atualiza contador de palavras
 	var conteudo = campo.val();
 
@@ -26,9 +40,10 @@ campo.on("input", function() {
 	$("#contador-caracteres").text(qtdCaracteres);
 
 });
+}
 
+function inicializaCronometro() {
     var tempoRestante = $("#tempo-digitacao").text();
-    
     //Ativa o cronometro assim que houver focus no textarea
     campo.one("focus", function() {
     var cronometroID = setInterval(function() {
@@ -42,3 +57,18 @@ campo.on("input", function() {
         }
     }, 1000);
 });
+}
+
+function reiniciaJogo(){
+    campo.attr("disabled",false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro(); 
+}
+
+
+
+
+
