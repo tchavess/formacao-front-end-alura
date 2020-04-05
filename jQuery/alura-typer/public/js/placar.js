fc5cc3c43cbee6a5e640ae2431e292e2efe8e690
Thiago Chaves <thiago.chaves04@gmail.com>
2020-04-05 18:00:09 -0300

    Botao mostrar/esconder placar - animacoes do placar com jQuery

---
+++ jQuery/alura-typer/public/js/placar.js
@@ -0,0 +1,46 @@
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Seu-nome"
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+}
+
+function novaLinha(usuario, numPalavras) {
+    //Criando uma tag <tr></tr>
+    var linha = $("<tr>");
+    //Criando os tds
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+    //cria um <a> com um atributi href e adiciona a classe botao-remover
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    // Ícone dentro do <a>
+    link.append(icone);
+
+    // <a> dentro do <td>
+    colunaRemover.append(link);
+
+    // Os três <td> dentro do <tr>
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(event) {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
+
+$("#botao-placar").click(mostraPlacar);
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
+}
