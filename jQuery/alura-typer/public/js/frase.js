c7536624b7a4df9058d6bcb9b608d224fd03c115
Thiago Chaves <thiago.chaves04@gmail.com>
2020-04-07 22:31:49 -0300

    Busando frase no servidor pelo id com $.get()

--- jQuery/alura-typer/public/js/frase.js
+++ jQuery/alura-typer/public/js/frase.js
@@ -1,8 +1,19 @@
+$("#botao-frase-id").click(buscaFrase);
+
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
 
-    $.get('http://localhost:3000/frases',trocaFraseAleatoria);
+	$("#spinner").toggle(); //novo, mostrando o spinner
+
+    $.get('http://localhost:3000/frases',trocaFraseAleatoria).fail(function() {
+    	$("#erro").toggle(); //ao falhar mostra a mensagem de erro
+    	setTimeout(function(){
+            $("#erro").toggle();
+        },1500);
+    }).always(function(){ // novo, escondendo o spinner
+        $("#spinner").toggle();
+	});
 }
 
 function trocaFraseAleatoria(data) {
@@ -13,4 +24,35 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+
+	$("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    //criacao do objeto JS que guarda a id
+    var dados = {id : fraseId}; 
+
+    //passando objecto como segundo parametro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+    },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
